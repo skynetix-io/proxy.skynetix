18fc36418fd5dea6283bf9d7ff098ebfd13ec32d
bigdrd <e.luciani@protonmail.com>
2023-04-21 14:38:16 +0200

    ok

--- run.sh
+++ run.sh
@@ -5,5 +5,7 @@ export FLASK_export=production
 export FLASK_DEBUG=0
 export SECRET_KEY='asdasvasv77sa0970932=/(6'
 
-
-gunicorn --bind 0.0.0.0:80 -w 5 app:app
+while true
+do
+    gunicorn --bind 0.0.0.0:80 -w 5 app:app
+done
